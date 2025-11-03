bfdf91592c4f2b2ca07b4ab65e707916e2b3c17b
sergijburacuk742-wq <sergijburacuk742@gmail.com>
2025-11-03 15:06:34 +0200

    Create laba2.py

---
+++ laba2.py
@@ -0,0 +1,30 @@
+age = 16
+weight = 79.4
+list_fruits = ['android', 'orange','lemon']
+list_cars = ['bmw','mercedes','audi']
+tuple_digits = (1,2,3,4,5,6,7,8,9,0)
+it_is_python = True
+unique_numbers = {14,87,94,881}
+height = 189
+students_in_group = 21
+name = 'Serhiy'
+print(age)
+print(weight)
+print(list_fruits)
+print(list_cars)
+print(tuple_digits)
+print(it_is_python)
+print(unique_numbers)
+print(height)
+print(students_in_group)
+print(name)
+print(type(age))
+print(type(weight))
+print(type(list_fruits))
+print(type(list_cars))
+print(type(tuple_digits))
+print(type(it_is_python))
+print(type(unique_numbers))
+print(type(height))
+print(type(students_in_group))
+print(type(name))
