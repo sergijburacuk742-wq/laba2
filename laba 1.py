b9e29524f699529817da13e71930285fdc7ade99
sergijburacuk742-wq <sergijburacuk742@gmail.com>
2025-11-24 20:39:20 +0200

    Create laba 1.py

---
+++ laba 1.py
@@ -0,0 +1,32 @@
+distance = 3
+Pi = 3.14
+mood = "Happy"
+scores = [65, 42, 73, 94, 82]
+is_open = True
+coords = (50.45, 30.52, 73.28)
+signal = 2 + 3j
+fruits = {"apple", "banana", "orange"}
+student = {"name": "Anna", "age": 17}
+passport_number = None
+
+print(distance)
+print(Pi)
+print(mood)
+print(scores)
+print(is_open)
+print(coords)
+print(signal)
+print(fruits)
+print(student)
+print(passport_number)
+
+print(type(distance))
+print(type(Pi))
+print(type(mood))
+print(type(scores))
+print(type(is_open))
+print(type(coords))
+print(type(signal))
+print(type(fruits))
+print(type(student))
+print(type(passport_number))
